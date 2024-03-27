1a7dd4031cacec72ccdef8436facd5852c761cd8
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-03-27 13:02:18 +0530

    Update approve.py

--- JARVISROBO/plugins/approve.py
+++ JARVISROBO/plugins/approve.py
@@ -253,7 +253,7 @@ dispatcher.add_handler(APPROVAL)
 dispatcher.add_handler(UNAPPROVEALL)
 dispatcher.add_handler(UNAPPROVEALL_BTN)
 
-__mod_name__ = "ᴀᴘᴘʀᴏᴠᴀʟs"
+__mod_name__ = "Aᴘᴘʀᴏᴠᴀʟs"
 __command_list__ = ["approve", "unapprove", "approved", "approval"]
 __handlers__ = [APPROVE, DISAPPROVE, APPROVED, APPROVAL]
 # <================================================ END =======================================================>
